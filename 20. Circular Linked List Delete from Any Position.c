c99ae08bfbf4254c38b1c097a3dc97bd33b285cf
ahsansadik <ahsansadik124@gmail.com>
2025-03-01 21:44:37 +0600

    Update 20. Circular Linked List Delete from Any Position.c

--- 20. Circular Linked List Delete from Any Position.c
+++ 20. Circular Linked List Delete from Any Position.c
@@ -18,54 +18,51 @@ void printList(struct Node* head)
     struct Node *temp = head;
     while(temp->next != head)
     {
-        printf("%d -> ",temp -> data);
+        printf("%d  ",temp -> data);
         temp = temp->next;
     }
-     printf("%d (head)\n",temp -> data);
+     printf("%d \n",temp -> data);
 }
 
 struct Node* deleteFromPosition(struct Node *head, int position)
 {
-    if (head == NULL)
-        return NULL;
-
-    if (position == 1)  // If deleting the first node
+    if (position == 1)
     {
-        if (head->next == head)  // Only one node in the list
+        if (head->next == head)
         {
             free(head);
             return NULL;
         }
 
         struct Node *temp = head;
-        struct Node *last = head;
+        struct Node *temp2 = head;
 
-        while (last->next != head)  // Find last node
+        while (temp2->next != head)
         {
-            last = last->next;
+            temp2 = temp2->next;
         }
 
-        head = head->next;  // Move head to next node
-        last->next = head;  // Update last node's next pointer
+        head = head->next;
+        temp2->next = head;
 
-        free(temp);  // Delete old head
+        free(temp);
         return head;
     }
 
     struct Node *temp = head;
-    struct Node *prev = NULL;
+    struct Node *temp2 = NULL;
 
     for (int i = 1; i < position && temp->next != head; i++)
     {
-        prev = temp;
+        temp2 = temp;
         temp = temp->next;
     }
 
-    if (temp->next == head && position > 1)  // If position is out of range
+    if (temp->next == head && position > 1)
         return head;
 
-    prev->next = temp->next;  // Bypass the node
-    free(temp);  // Delete the node
+    temp2->next = temp->next;
+    free(temp);
 
     return head;
 }
@@ -76,30 +73,27 @@ int main()
     struct Node *one = NULL;
     struct Node *two = NULL;
     struct Node *three = NULL;
-    struct Node *four = NULL;
 
     one = malloc(sizeof(struct Node));
     two = malloc(sizeof(struct Node));
     three = malloc(sizeof(struct Node));
-    four = malloc(sizeof(struct Node));
 
     one->data = 1;
     two->data = 2;
     three->data = 3;
-    four->data = 4;
 
     head = one;
 
     one->next = two;
     two->next = three;
-    three->next = four;
-    four->next = head; // Making it circular
+    three->next = head;
 
     printList(head);
 
-    head = deleteFromPosition(head, 2);  // Delete node at position 2
+    head = deleteFromPosition(head, 2);
 
     printList(head);
 
     return 0;
 }
+
