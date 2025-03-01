b16b08231cf578b56331157c685a640b9ee26235
ahsansadik <ahsansadik124@gmail.com>
2025-03-01 20:55:03 +0600

    Update 17. Circular Linked List Insert at Any Position.c

--- 17. Circular Linked List Insert at Any Position.c
+++ 17. Circular Linked List Insert at Any Position.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct Node{
     int data;
     struct Node *next;
 };
 
-struct Node *insertAtPosition (struct Node* head, int data, int position)
+void printList(struct Node* head)
 {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node *temp = head;
+    while(temp->next != head)
+    {
+        printf("%d ",temp -> data);
+        temp = temp->next;
+    }
+     printf("%d \n", temp -> data);
+}
+
+struct Node *insertAtPosition (struct Node *head, int data, int position)
+{
+    struct Node* newNode = malloc(sizeof(struct Node));
+
     newNode->data = data;
     newNode->next=NULL;
 
@@ -31,46 +50,35 @@ struct Node *insertAtPosition (struct Node* head, int data, int position)
     }
 
     struct Node* temp = head;
-    int count = 1;
 
-    while(count  < position - 1  && temp -> next != head)
+
+    for(int i = 1; i  < position - 1  && temp -> next != head; i++)
     {
         temp = temp->next;
-        count++;
     }
 
     newNode->next = temp->next;
     temp->next=newNode;
+
     return head;
 }
 
-void printList(struct Node* head)
-{
-    if (head == NULL)
-    {
-        printf("List is empty\n");
-        return;
-    }
-
-    struct Node *temp = head;
-    while(temp->next != head)
-    {
-        printf("%d -> ",temp -> data);
-        temp = temp->next;
-    }
-     printf("%d (head)\n",temp -> data);
-}
 
 int main()
 {
     struct Node *head = NULL;
+
+    printList(head);
+
     head = insertAtPosition(head, 10, 1);
+    printList(head);
+
     head = insertAtPosition(head, 15, 2);
+    printList(head);
+
     head = insertAtPosition(head, 20, 3);
-
-    printf("Circular Linked List\n");
-
     printList(head);
 
     return 0;
 }
+
